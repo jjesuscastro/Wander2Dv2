7abe1a002532cc157b8c46f7af000bc574dd3592
jjesuscastro <joesei_castro@dlsu.edu.ph>
2019-03-25 19:01:33 +0800

    fixed slope movement

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 namespace Player
 {
     [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(CircleCollider2D))]
     public class CharacterController2D : MonoBehaviour
     {
+        public float maxClimbSlope = 45;
+        RaycastOrigins raycastOrigins;
+        public int horizontalRayCount = 4;
+        public int verticalRayCount = 4;
+        float horizontalRaySpacing;
+        float verticalRaySpacing;
+
         [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
         [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
         [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
+        [SerializeField] private BoxCollider2D m_SlopeCheck;                        // A collider that will check the slope to climb
         [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
         [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
         [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
@@ -32,6 +41,11 @@ namespace Player
 
         public BoolEvent OnCrouchEvent;
         private bool m_wasCrouching = false;
+
+        private void Start()
+        {
+            CalculateRaySpacing();
+        }
 
         private void Awake()
         {
@@ -63,9 +77,92 @@ namespace Player
             }
         }
 
+        void HorizontallCollisions(ref Vector3 velocity)
+        {
+            float directionX = Mathf.Sign(velocity.x);
+            float rayLength = Mathf.Abs(velocity.x);
+
+            for(int i = 0; i < horizontalRayCount; i++)
+            {
+                Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+                rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, 0.5f, m_WhatIsGround);
+
+                Debug.DrawRay(rayOrigin, Vector2.right * directionX * 0.5f, Color.red);
+
+                if(hit)
+                {
+                    //We only want to use this for the slopeChecking not the movement so use this only for slope checking
+                    float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                    Debug.Log(hit.collider.name + " " + slopeAngle);
+                    if(i == 0 && slopeAngle <= maxClimbSlope)
+                    {
+                        ClimbSlope(ref velocity, slopeAngle);
+                    }
+                    rayLength = hit.distance;
+                }
+            }
+        }
+
+        // void VerticalCollisions(ref Vector3 velocity)
+        // {
+        //     float directionY = Mathf.Sign(velocity.y);
+        //     float rayLength = Mathf.Abs(velocity.y);
+
+        //     for(int i = 0; i < verticalRayCount; i++)
+        //     {
+        //         Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+        //         rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+        //         RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, m_WhatIsGround);
+
+        //         Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+        //         if(hit)
+        //         {
+        //             //We only want to use this for the slopeChecking not the movement so use this only for slope checking
+        //             Debug.Log("Ground detected");
+        //             rayLength = hit.distance;
+        //         }
+        //     }
+        // }
+
+        void ClimbSlope(ref Vector3 velocity, float slopeAngle)
+        {
+            float moveDistance = Mathf.Abs(velocity.x);
+            float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+            if(velocity.y <= climbVelocityY)
+            {
+                velocity.y = climbVelocityY;
+                velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+                m_Grounded = true;
+            }
+        }
+
+        void UpdateRaycastOrigins()
+        {
+            Bounds bounds = m_SlopeCheck.bounds;
+
+            raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+            raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+            raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+            raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+        }
+
+        void CalculateRaySpacing()
+        {
+            Bounds bounds = m_SlopeCheck.bounds;
+
+            horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+            verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+            horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+            verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+        }
 
         public void Move(float move, bool crouch, bool jump)
         {
+            UpdateRaycastOrigins();
             // If crouching, check to see if the character can stand up
             if (!crouch)
             {
@@ -111,6 +208,11 @@ namespace Player
 
                 // Move the character by finding the target velocity
                 Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+                if(targetVelocity.x != 0)
+                    HorizontallCollisions(ref targetVelocity);
+
+                // if(targetVelocity.y != 0)
+                //     VerticalCollisions(ref targetVelocity);
                 // And then smoothing it out and applying it to the character
                 m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
@@ -146,6 +248,12 @@ namespace Player
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
+        }
+
+        struct RaycastOrigins
+        {
+            public Vector2 topLeft, topRight;
+            public Vector2 bottomLeft, bottomRight;
         }
     }
 }
