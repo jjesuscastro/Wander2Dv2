7917cb5c909798271f53abb40152a10be10ef5b3
jjesuscastro <joesei_castro@dlsu.edu.ph>
2019-03-28 23:22:35 +0800

    added dialogue trigger

---
+++ Assets/Scripts/Objects/DialogueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameManager;
+
+namespace Object
+{
+    public class DialogueTrigger : Interactable
+    {
+        [SerializeField]
+        private Dialogue dialogue;
+
+        public override bool Interact()
+        {
+            TriggerDialogue();
+
+            return true;
+        }
+
+        void TriggerDialogue()
+        {
+            dialogue.TriggerDialogue();
+            Destroy(gameObject);
+        }
+    }
+}
+
