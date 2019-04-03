1235a0993681b67acccdc848a314ae134780c45a
jjesuscastro <joesei_castro@dlsu.edu.ph>
2019-04-03 18:02:44 +0800

    added thunderstorm

---
+++ Assets/Scripts/Environment/ThunderRandom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderRandom : MonoBehaviour
+{
+    public Transform thunderManager;
+    public GameObject[] thunders;
+    public float minX;
+    public float maxX;
+    public float fixedY;
+    public float minInterval;
+    public float maxInterval;
+
+    // Update is called once per frame
+    void Start()
+    {
+        Invoke("SpawnThunder", 0);
+    }
+
+    void SpawnThunder()
+    {
+        float interval = Random.Range(minInterval, maxInterval);
+        float x = Random.Range(minX, maxX);
+        Vector3 camera = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        int objToSpawn = Random.Range(0, thunders.Length);
+
+        GameObject newObj = Instantiate(thunders[objToSpawn], new Vector3(x, camera.y/2.5f, 0), Quaternion.identity);
+        newObj.transform.SetParent(thunderManager);
+
+        Debug.Log("Interval = " + interval);
+        Invoke("SpawnThunder", interval);
+    }
+}
---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    public float destroyIn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("DestroySelf", destroyIn);
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
