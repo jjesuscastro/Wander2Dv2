3304b26ec184f8d7d736957819b54a5fe664fada
jjesuscastro <joesei_castro@dlsu.edu.ph>
2019-04-03 21:52:23 +0800

    thunderstorm now has on-off bool

--- Assets/Scripts/Environment/ThunderRandom.cs
+++ Assets/Scripts/Environment/ThunderRandom.cs
@@ -6,6 +6,8 @@ public class ThunderRandom : MonoBehaviour
 {
     public Transform thunderManager;
     public GameObject[] thunders;
+    public bool isEnabled = false;
+    private bool firstThunder = true;
     public float minX;
     public float maxX;
     public float fixedY;
@@ -13,22 +15,30 @@ public class ThunderRandom : MonoBehaviour
     public float maxInterval;
 
     // Update is called once per frame
-    void Start()
+    void Update()
     {
-        Invoke("SpawnThunder", 0);
+        if(isEnabled && firstThunder)
+        {
+            Invoke("SpawnThunder", 0);
+            firstThunder = false;
+        }
+
+        if(!isEnabled)
+            firstThunder = true;
     }
 
     void SpawnThunder()
     {
-        float interval = Random.Range(minInterval, maxInterval);
-        float x = Random.Range(minX, maxX);
-        Vector3 camera = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        int objToSpawn = Random.Range(0, thunders.Length);
+        if(isEnabled)
+        {
+            float interval = Random.Range(minInterval, maxInterval);
+            float x = Random.Range(minX, maxX);
+            Vector3 camera = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            int objToSpawn = Random.Range(0, thunders.Length);
 
-        GameObject newObj = Instantiate(thunders[objToSpawn], new Vector3(x, camera.y/2.5f, 0), Quaternion.identity);
-        newObj.transform.SetParent(thunderManager);
-
-        Debug.Log("Interval = " + interval);
-        Invoke("SpawnThunder", interval);
+            GameObject newObj = Instantiate(thunders[objToSpawn], new Vector3(x, camera.y/2.5f, 0), Quaternion.identity);
+            newObj.transform.SetParent(thunderManager);
+            Invoke("SpawnThunder", interval);
+        }
     }
 }
