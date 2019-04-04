c9eb41eca00799bca18f730811dac81a1beae1e4
jjesuscastro <joesei_castro@dlsu.edu.ph>
2019-04-04 18:36:12 +0800

    made flicker random script

---
+++ Assets/Scripts/Environment/FlickerRandom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerRandom : MonoBehaviour
+{
+
+    public float minTime;
+    public float maxTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delay = Random.Range(minTime, maxTime);
+        Invoke("Toggle", delay);
+    }
+
+    void Toggle()
+    {
+        if(gameObject.active)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+}
