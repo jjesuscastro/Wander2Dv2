32764879878f203a61fc8af2b1a80199cd5768e9
jjesuscastro <joesei_castro@dlsu.edu.ph>
2019-04-05 13:29:01 +0800

    asd

---
+++ Assets/Scripts/Environment/HorizontalPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalPlatform : MonoBehaviour
+{
+    public float maxX;
+    public float minX;
+    public float speed;
+    public bool moveRight = true;
+    public bool oneDirection = false;
+    public bool stop = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!stop)
+        {
+            if(transform.localPosition.x >= maxX)
+            {
+                moveRight = false;
+                if(oneDirection)
+                    stop = true;
+            }
+            if(transform.localPosition.x <= minX)
+                moveRight = true;
+
+            if(moveRight)
+                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+            else
+                transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+    }
+}
