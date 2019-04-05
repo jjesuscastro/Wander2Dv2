97cd9a31077a401af2b3bbb559038d6ed19cf40e
jjesuscastro <joesei_castro@dlsu.edu.ph>
2019-04-05 09:40:36 +0800

    made popup

---
+++ Assets/Scripts/UI/PopupNotification.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class PopupNotification : MonoBehaviour
+    {
+        public float fadeRate;
+        public Image popup;
+        public TextMeshProUGUI popupText;
+        public bool showUp = false;
+
+        #region Singleton
+        public static PopupNotification instance;
+
+        void Awake()
+        {
+            if(instance != null)
+            {
+                Debug.LogWarning("Multiple popup notifications found");
+            }
+            instance = this;
+        }
+        #endregion
+
+        void Update()
+        {
+            Color tempColorPopup, tempColorText;
+            if(showUp)
+            {
+                Debug.Log("ey");
+                tempColorPopup = popup.color;
+                tempColorPopup.a += fadeRate;
+
+                if(tempColorPopup.a <= 0.33f)
+                    popup.color = tempColorPopup;
+
+                tempColorText = popupText.color;
+                tempColorText.a += fadeRate * 3;
+
+                if(tempColorText.a <= 1f)
+                    popupText.color = tempColorText;
+                else
+                    Invoke("Hide", 1.5f);
+            } else {
+                tempColorPopup = popup.color;
+                tempColorPopup.a -= fadeRate;
+
+                popup.color = tempColorPopup;
+
+                tempColorText = popupText.color;
+                tempColorText.a -= fadeRate * 3;
+                popupText.color = tempColorText;
+            }
+        }
+
+        public void ShowPopup(string newPopupText)
+        {
+            popupText.text = newPopupText;
+            showUp = true;
+        }
+
+        void Hide()
+        {
+            showUp = false;
+        }
+    }
+}
