7593d86397c22ba6604ce280ed3bbe540e3d05c4
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-02 22:31:19 +0800

    Added blank png
    added blank png

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class Cutscene : MonoBehaviour
 {
     public SpriteRenderer blackCover;
+    public SpriteRenderer sceneRenderer;
     public Sprite[] cutscenes;
 
     int currScene = 0;
+    bool sceneStarting = false;
+
+    void Update()
+    {
+        
+    }
+
+    public void StartScene() {
+        Color color = blackCover.color;
+        color.a = 0;
+        blackCover.color = color;
+        sceneStarting = true;
+    }
 
     public void NextScene() {
         if(currScene != cutscenes.Length)
         {
-            
+            sceneRenderer.sprite = cutscenes[currScene];
+            currScene++;
         }
     }
 }
