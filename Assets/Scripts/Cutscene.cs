c330370023b147be446f87e2cfc2a54d7a9fe2d6
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-02 17:11:50 +0800

    added cutscene sprites and created cutscene script

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    public SpriteRenderer blackCover;
+    public Sprite[] cutscenes;
+
+    int currScene = 0;
+
+    public void NextScene() {
+        if(currScene != cutscenes.Length)
+        {
+            
+        }
+    }
+}
