bc2fa8572acba62ba48b5b848b097cbe3b2b8d49
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-03 15:07:16 +0800

    main menu cutscene done

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -8,16 +8,20 @@ public class Cutscene : MonoBehaviour
     public UnityEvent onBlackCover;
     public SpriteRenderer blackCover;
     public SpriteRenderer sceneRenderer;
+    public SpriteRenderer blankSceneRenderer;
     public Sprite[] cutscenes;
 
-    int currScene = 0;
+    public int currScene = 0;
     float timer = 0;
+    float sceneTimer = 0;
     bool sceneStarting = false;
-    bool startTimer = true;
+    bool startTimer = false;
+    public bool scenePlaying = false;
+    bool sceneChanging = false;
 
     void Update()
     {
-        if (sceneStarting)
+        if (sceneStarting && !scenePlaying)
         {
             Color color = blackCover.color;
             color.a += 0.025f;
@@ -26,7 +30,7 @@ public class Cutscene : MonoBehaviour
             startTimer = true;
         }
 
-        if (startTimer)
+        if (startTimer && !scenePlaying)
         {
             if (timer % 60 > 1)
             {
@@ -44,8 +48,41 @@ public class Cutscene : MonoBehaviour
         {
             Color color = blackCover.color;
             color.a -= 0.025f;
-            if (color.a > 0)
+            if (color.a >= 0)
                 blackCover.color = color;
+        }
+
+        if (scenePlaying && !sceneChanging)
+        {
+            Color color = blankSceneRenderer.color;
+            color.a -= 0.025f;
+            if (color.a >= 0)
+                blankSceneRenderer.color = color;
+
+            sceneTimer += Time.deltaTime;
+            if (sceneTimer % 60 > 5)
+            {
+                sceneTimer = 0;
+                sceneChanging = true;
+            }
+        }
+
+        if (scenePlaying && sceneChanging)
+        {
+            Color color = blankSceneRenderer.color;
+            color.a += 0.025f;
+            blankSceneRenderer.color = color;
+
+            if (color.a <= 1)
+                blankSceneRenderer.color = color;
+
+            sceneTimer += Time.deltaTime;
+            if (sceneTimer % 60 > 1)
+            {
+                sceneTimer = 0;
+                NextScene();
+                sceneChanging = false;
+            }
         }
     }
 
@@ -63,6 +100,11 @@ public class Cutscene : MonoBehaviour
         {
             sceneRenderer.sprite = cutscenes[currScene];
             currScene++;
+            scenePlaying = true;
+        }
+        else
+        {
+            scenePlaying = false;
         }
     }
 }
