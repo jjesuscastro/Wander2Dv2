8c7fd8bb07beceb22b9ecc86129a799affe45088
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-03 11:44:35 +0800

    Added cutscenes
    can't go to next scene yet

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Cutscene : MonoBehaviour
 {
+    public UnityEvent onBlackCover;
     public SpriteRenderer blackCover;
     public SpriteRenderer sceneRenderer;
     public Sprite[] cutscenes;
 
     int currScene = 0;
+    float timer = 0;
     bool sceneStarting = false;
+    bool startTimer = true;
 
     void Update()
     {
@@ -17,6 +21,31 @@ public class Cutscene : MonoBehaviour
         {
             Color color = blackCover.color;
             color.a += 0.025f;
+            blackCover.color = color;
+            timer += Time.deltaTime;
+            startTimer = true;
+        }
+
+        if (startTimer)
+        {
+            if (timer % 60 > 1)
+            {
+                if (onBlackCover != null)
+                {
+                    onBlackCover.Invoke();
+                }
+                sceneStarting = false;
+                timer = 0;
+                startTimer = false;
+            }
+        }
+
+        if (!sceneStarting)
+        {
+            Color color = blackCover.color;
+            color.a -= 0.025f;
+            if (color.a > 0)
+                blackCover.color = color;
         }
     }
 
