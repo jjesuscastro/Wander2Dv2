f7dad606048b4618224589cadde7bf12d80c5774
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-06 23:28:22 +0800

    added prompt to press E to talk to npc

--- Assets/Scripts/UI/JournalUI.cs
+++ Assets/Scripts/UI/JournalUI.cs
@@ -21,7 +21,7 @@ namespace UI
 
         public void ToggleUI()
         {
-            // currentPage = 0; uncomment to always start at cover page
+            currentPage = 0; uncomment to always start at cover page
             center.gameObject.SetActive(true);
             left.gameObject.SetActive(false);
             right.gameObject.SetActive(false);
