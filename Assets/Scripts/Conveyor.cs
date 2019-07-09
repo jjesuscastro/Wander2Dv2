88549c7559fbf37662510ef8b58d621630ed016e
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-10 05:36:07 +0800

    moving conveyor

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    Vector3 tempVector;
+    public float speed;
+
+    void Update()
+    {
+        foreach(Transform child in transform)
+        {
+            tempVector = child.transform.position;
+            tempVector.x += speed * Time.deltaTime;
+            child.transform.position = tempVector;            
+        }
+    }
+}
