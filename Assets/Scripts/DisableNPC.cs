11f1bfccc66f893f2a29854f673577c455727020
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-12 08:28:34 +0800

    w2 puzzle

---
+++ Assets/Scripts/DisableNPC.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DisableNPC : MonoBehaviour
+{
+    public PlayerMovement npc;
+    public bool disableNPC = true;
+
+    void DisNPC()
+    {
+        npc.enabled = false;
+    }
+
+    void EnableNPC()
+    {
+        npc.enabled = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("NPC"))
+        {
+            if(disableNPC)
+                DisNPC();
+            else
+                EnableNPC();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 0, .5f);
+        Gizmos.DrawCube(GetComponent<BoxCollider2D>().bounds.center, new Vector2(GetComponent<BoxCollider2D>().transform.localScale.x, GetComponent<BoxCollider2D>().transform.localScale.y));
+    }
+}
