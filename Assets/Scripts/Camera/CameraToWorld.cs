04c486771ce3c990aae60a88ec810406e5df2c93
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-14 22:55:59 +0800

    added plater switch

--- Assets/Scripts/Camera/CameraToWorld.cs
+++ Assets/Scripts/Camera/CameraToWorld.cs
@@ -4,50 +4,50 @@ using UnityEngine;
 
 public class CameraToWorld : MonoBehaviour
 {
-    Camera camera;
+    Camera cam;
     void Awake()
     {
-        camera = GetComponent<Camera>();
+        cam = GetComponent<Camera>();
     }
 
     void OnDrawGizmos()
     {
-        camera = GetComponent<Camera>();
-        
-        Vector3 upperRight = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        cam = GetComponent<Camera>();
+
+        Vector3 upperRight = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(upperRight, 1F);
 
-        Vector3 upperLeft = camera.ViewportToWorldPoint(new Vector3(0, 1, camera.nearClipPlane));
+        Vector3 upperLeft = cam.ViewportToWorldPoint(new Vector3(0, 1, cam.nearClipPlane));
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(upperLeft, 1F);
 
-        Vector3 lowerRight = camera.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
+        Vector3 lowerRight = cam.ViewportToWorldPoint(new Vector3(1, 0, cam.nearClipPlane));
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(lowerRight, 1F);
 
-        Vector3 lowerLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        Vector3 lowerLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(lowerLeft, 1F);
     }
 
     public Vector3 UpperRight()
     {
-        return camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        return cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
     }
 
     public Vector3 UpperLeft()
     {
-        return camera.ViewportToWorldPoint(new Vector3(0, 1, camera.nearClipPlane));
+        return cam.ViewportToWorldPoint(new Vector3(0, 1, cam.nearClipPlane));
     }
 
     public Vector3 LowerRight()
     {
-        return camera.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
+        return cam.ViewportToWorldPoint(new Vector3(1, 0, cam.nearClipPlane));
     }
 
     public Vector3 LowerLeft()
     {
-        return camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        return cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
     }
 }
