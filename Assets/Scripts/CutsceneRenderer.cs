cdd4be7af618895fc5d3ecbea699a35d6a59c1ab
jjoeseicastro <jjoesei.castro@gmail.com>
2019-07-14 18:03:46 +0800

    added w1 cutscenes

---
+++ Assets/Scripts/CutsceneRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneRenderer : MonoBehaviour
+{
+    #region Singleton
+        public static CutsceneRenderer instance;
+
+        void Awake()
+        {
+            if (instance != null)
+            {
+                Debug.LogWarning("Multiple cutscene renderers found");
+            }
+            instance = this;
+        }
+    #endregion
+}
