c1d1ac6044ad3245bca2c7b1328438a24b6776d4
jjoeseicastro <jjoesei.castro@gmail.com>
2019-08-10 21:37:24 +0800

    added trigger for alarm

---
+++ Assets/Scripts/AlarmTrigger.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmTrigger : MonoBehaviour
+{
+    public bool turnOn = true;
+    [SerializeField]
+    private SpriteRenderer redOverlay;
+    float fadeRate = 0.025f;
+    bool delay = false;
+    bool triggered = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!delay && triggered){
+            if(turnOn){
+                if(redOverlay != null)
+                {
+                    Color tempColor = redOverlay.color;
+                    tempColor.a += fadeRate;
+                    redOverlay.color = tempColor;
+                    if(redOverlay.color.a >= 0.25f)
+                        delay = true;
+                }
+            } else {
+                if(redOverlay != null)
+                {
+                    Color tempColor = redOverlay.color;
+                    tempColor.a -= fadeRate;
+                    redOverlay.color = tempColor;
+
+                    if(redOverlay.color.a <= 0){
+                        tempColor.a = 0;
+                        redOverlay.color = tempColor;
+                    }
+                }
+            }
+        } else if (delay)
+        {
+            if(redOverlay != null)
+            {
+                Color tempColor = redOverlay.color;
+                tempColor.a -= fadeRate;
+                redOverlay.color = tempColor;
+
+                if(redOverlay.color.a <= 0){
+                    tempColor.a = 0;
+                    redOverlay.color = tempColor;
+                    delay = false;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("NPC"))
+        {
+            triggered = true;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, .5f);
+        Gizmos.DrawCube(GetComponent<BoxCollider2D>().bounds.center, new Vector2(GetComponent<BoxCollider2D>().transform.localScale.x, GetComponent<BoxCollider2D>().transform.localScale.y));
+    }
+}
