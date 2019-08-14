6b5a7850213c5eface7dc084a09eae33d25a5219
jjoeseicastro <jjoesei.castro@gmail.com>
2019-08-14 11:04:59 +0800

    thunderstorm

---
+++ Assets/Scripts/ThunderFlash.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderFlash : MonoBehaviour
+{
+    public float fadeRate;
+    public float minDisabledTime;
+    public float maxDisabledTime;
+    public GameObject whiteCover;
+    bool firstCall = true;
+    bool isEnabled = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isEnabled)
+        {
+            Color color = whiteCover.GetComponent<SpriteRenderer>().color;
+
+            color.a -= fadeRate;
+            if (color.a <= 0)
+                color.a = 0;
+
+            whiteCover.GetComponent<SpriteRenderer>().color = color;
+
+            if (firstCall)
+            {
+                float delay = 10;
+                delay = Random.Range(minDisabledTime, maxDisabledTime);
+                Invoke("Toggle", delay);
+                firstCall = false;
+            }
+        }
+    }
+
+    void Toggle()
+    {
+        Color color = whiteCover.GetComponent<SpriteRenderer>().color;
+        color.a = 0.85f;
+        whiteCover.GetComponent<SpriteRenderer>().color = color;
+
+        float delay = 10;
+        delay = Random.Range(minDisabledTime, maxDisabledTime);
+
+        if(isEnabled)
+            Invoke("Toggle", delay);
+    }
+
+    public void EnableStorm()
+    {
+        isEnabled = true;
+    }
+
+    public void DisableStorm()
+    {
+        isEnabled = false;
+    }
+}
